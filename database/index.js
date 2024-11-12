58eeb1208f15a0f3fb7eac9c50962fb6de3f3437
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-11-12 10:47:22 +0530

    addandget

--- database/index.js
+++ database/index.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const connection = require("./mongoose/db");
+const UserModel = require("./model/userModel");
 let port = 8080;
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
-  res.render("addData");
+app.get("/", async (req, res) => {
+  try {
+    const userData = await UserModel.find({});
+
+    res.render("addData", { userData });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/insertAddData", (req, res) => {
-  console.log(req.body);
-  res.redirect("back")
+app.post("/insertAddData", async (req, res) => {
+  // console.log(req.body);
+  // const { userName, password } = req.body;
+  // console.log(userName, password);
+  try {
+    await UserModel.create(req.body);
+    console.log("Data inserted successfully");
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.redirect("back");
 });
 
 app.listen(port, (error) => {
